d0d6bf8ff9ae9a91b0b15c66b27bd76e67044dc8
Rhys <rsimpson@amplience.com>
2019-11-18 16:00:09 +0000

    Fix [CATS-1177], [CATS-1179].

--- src/app/preview/crop-toolbar/crop-toolbar.component.ts
+++ src/app/preview/crop-toolbar/crop-toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DiFieldService } from 'src/app/editor/di-field.service';
+import { DiImageService } from 'src/app/editor/di-image.service';
 
 @Component({
   selector: 'amp-crop-toolbar',
@@ -9,21 +10,42 @@ import { DiFieldService } from 'src/app/editor/di-field.service';
 export class CropToolbarComponent implements OnInit {
 
   focus: boolean[] = [false, false, false, false];
+  crop: number[] = [0, 0, 0, 0];
 
-  get crop(): number[] {
-    return this.field.data == null ? [0, 0, 0, 0] : this.field.data.crop;
+  constructor(private field: DiFieldService, private image: DiImageService) {
+    this.readCrop();
+    field.fieldUpdated.subscribe(data => {
+      this.readCrop();
+    });
   }
-
-  set crop(value: number[]) {
-    this.field.data.crop = value;
-  }
-
-  constructor(private field: DiFieldService) { }
 
   ngOnInit() {
   }
 
-  updateCrop() {
+  readCrop() {
+    if (this.field.data == null) {
+      this.crop = [0, 0, 0, 0];
+    } else {
+      this.crop = this.field.data.crop.slice(0);
+    }
+  }
+
+  updateCrop(sizeUpdate: boolean) {
+    // if position is changed, bound the position based on the size
+    // if size is changed, do the opposite!
+    const bounds = this.image.getRotatedBounds();
+    const crop = this.crop;
+    if (sizeUpdate) {
+      // size is bounded to the crop bounds minus the position
+      crop[2] = Math.max(1, Math.min(crop[2], (bounds[0] + bounds[2]) - crop[0]));
+      crop[3] = Math.max(1, Math.min(crop[3], (bounds[1] + bounds[3]) - crop[1]));
+    } else {
+      // location is bounded to the crop bounds minus the size
+      crop[0] = Math.max(bounds[0], Math.min(crop[0], bounds[2] - crop[2]));
+      crop[1] = Math.max(bounds[1], Math.min(crop[1], bounds[3] - crop[3]));
+    }
+
+    this.field.data.crop = crop.slice(0);
     this.field.updateField();
   }
 
