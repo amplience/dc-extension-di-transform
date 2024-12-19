3f94d4c4920f549470993996203d6b4b0f086e81
Simona Demarco <simonapiademarco@gmail.com>
2024-12-19 15:27:09 +0000

    feat: bump image-studio-sdk to v0.5.1 add event listener logic

--- src/app/image-studio/image-studio.service.ts
+++ src/app/image-studio/image-studio.service.ts
@@ -3,14 +3,21 @@ import { DcSdkService } from '../api/dc-sdk.service';
 import { AmplienceImageStudio } from "@amplience/image-studio-sdk/dist/esm";
 import { DiFieldService } from '../editor/di-field.service';
 import { AssetLibraryService } from './asset-library.service';
+import {
+  ImageSaveEventData,
+  ImageStudioEventType,
+  SDKEventType,
+} from "@amplience/image-studio-sdk";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ImageStudioService {
-
-
-  constructor(private sdkService: DcSdkService, private assetLibraryService: AssetLibraryService, private diFieldService: DiFieldService) { }
+  constructor(
+    private sdkService: DcSdkService,
+    private assetLibraryService: AssetLibraryService,
+    private diFieldService: DiFieldService
+  ) {}
 
   public async openImageStudio(image) {
     try {
@@ -21,32 +28,40 @@ export class ImageStudioService {
       const srcImage = await this.assetLibraryService.getAssetById(image.id);
       const imageStudio = new AmplienceImageStudio({
         domain: imageStudioUrl,
+      }).withEventListener(ImageStudioEventType.ImageSave, async (data) => {
+        try {
+          const imageData = data as ImageSaveEventData;
+          if (imageData.image) {
+            const uploadedAsset = await this.assetLibraryService.uploadAsset(
+              imageData.image,
+              srcImage
+            );
+            const imageLink = this.assetLibraryService.createImageLinkFromAsset(
+              image,
+              uploadedAsset
+            );
+
+            this.diFieldService.updateImageValue(imageLink);
+            return SDKEventType.Success;
+          }
+          return SDKEventType.Fail;
+        } catch (error) {
+          console.error(error);
+          return SDKEventType.Fail;
+        }
       });
 
       if (sdkInstance.hub.organizationId) {
         imageStudio.withDecodedOrgId(sdkInstance.hub.organizationId);
       }
 
-      const studioResponse = await imageStudio.editImages([
+      await imageStudio.editImages([
         {
           url: srcImage.thumbURL,
           name: srcImage.name,
           mimeType: srcImage.mimeType,
         },
       ]);
-
-      if (studioResponse && studioResponse.image) {
-        const uploadedAsset = await this.assetLibraryService.uploadAsset(
-          studioResponse.image,
-          srcImage
-        );
-        const imageLink = this.assetLibraryService.createImageLinkFromAsset(
-          image,
-          uploadedAsset
-        );
-
-        this.diFieldService.updateImageValue(imageLink);
-      }
     } catch (e) {
       console.error("Image Studio error:", e);
     }
